7bdbe3c72c18bf3ba802c754285cda3e4810e24d
fredericolucio <fredvalentim.macedo@gmail.com>
2020-05-10 17:12:45 -0300

    implement metodsto implementing  methods getList() Login() and search() in class Usuarios

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$clarice = new Usuario();
+//Carregando um usuário
+//$clarice = new Usuario();
+//$clarice->loadbyId(2);
+//echo $clarice;
 
-$clarice->loadbyId(2);
+//carregando uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $clarice;
+
+//carrega uma lisata de usuarios buscando pelo login
+//$search = Usuario::search("a"); 
+//echo json_encode($search);
+
+//carrega um usuário usando senha e login
+$usuario = new Usuario;
+$usuario->login("Mariana","123456");
+echo $usuario;
+
 
 ?>
